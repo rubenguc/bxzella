9d61c02333e955ee65d797f512f6a931b85abb41
Ruben <rubdeivis@gmail.com>
2025-06-24 10:56:59 -0400

    feat: add some popovers to position detail dialog

--- src/features/trades/components/TradeInfoDialog.tsx
+++ src/features/trades/components/TradeInfoDialog.tsx
@@ -21,12 +21,19 @@ import { useTranslations } from "next-intl";
 import { formatDecimal } from "@/utils/number-utils";
 import { useUserConfigStore } from "@/store/user-config-store";
 import { Badge } from "@/components/ui/badge";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import { Info } from "lucide-react";
 
 interface InfoRowProps {
   label: string;
   value: React.ReactNode;
   className?: string;
   valueClassName?: string;
+  tooltipInfo?: string;
 }
 
 export function InfoRow({
@@ -34,10 +41,25 @@ export function InfoRow({
   value,
   className,
   valueClassName,
+  tooltipInfo,
 }: InfoRowProps) {
   return (
-    <div className={`flex items-center justify-between ${className || ""}`}>
-      <p className="font-medium">{label}</p>
+    <div
+      className={`flex items-center justify-between text-sm lg:text-base ${className || ""}`}
+    >
+      <div className="flex items-center gap-1.5">
+        <p className="font-medium">{label}</p>
+        {tooltipInfo && (
+          <Popover>
+            <PopoverTrigger asChild>
+              <Info className="text-gray-500 dark:text-gray-300" size={16} />
+            </PopoverTrigger>
+            <PopoverContent className="w-80">
+              <p className="text-balance text-sm">{tooltipInfo}</p>
+            </PopoverContent>
+          </Popover>
+        )}
+      </div>
       <p className={valueClassName}>{value}</p>
     </div>
   );
@@ -79,16 +101,22 @@ export function TradeInfoDialog() {
     4,
   );
 
-  const aproxEntryAmount = getRealPositionAmount({ avgPrice, positionAmt });
+  const aproxEntryAmount = formatDecimal(
+    getRealPositionAmount({ avgPrice, positionAmt }),
+  );
   const aproxEntryAmountWithLeverage = formatDecimal(
-    aproxEntryAmount / leverage,
+    Number(aproxEntryAmount) / leverage,
     2,
   );
-  const openPositionValue = getRealPositionAmount({ avgPrice, positionAmt });
-  const closePositionValue = getRealPositionAmount({
-    avgPrice: avgClosePrice,
-    positionAmt,
-  });
+  const openPositionValue = formatDecimal(
+    getRealPositionAmount({ avgPrice, positionAmt }),
+  );
+  const closePositionValue = formatDecimal(
+    getRealPositionAmount({
+      avgPrice: avgClosePrice,
+      positionAmt,
+    }),
+  );
   const openPositionAmountFormatted = formatSymbolAmount(positionAmt);
   const closePositionAmountFormatted = formatSymbolAmount(closePositionAmt);
 
@@ -108,10 +136,11 @@ export function TradeInfoDialog() {
               {transformTimeToLocalDate(updateTime)}
             </DialogDescription>
           </DialogHeader>
-          <div className="-mr-4 h-[26.25rem] w-full overflow-y-auto py-1 pr-4">
+          <div className="-mr-4 min-h-[26.25rem] w-full overflow-y-auto py-1 pr-4">
             <div className="flex flex-col gap-2">
               <InfoRow
                 label={t("position_pnl")}
+                tooltipInfo={t("position_pnl_info")}
                 value={`${netProfitFormatted} ${coin}`}
                 valueClassName={`font-bold ${isWin ? "text-green-500" : "text-red-500"}`}
                 className="text-xl"
@@ -119,6 +148,7 @@ export function TradeInfoDialog() {
 
               <InfoRow
                 label={t("realised_pnl")}
+                tooltipInfo={t("realised_pnl_info")}
                 value={`${realisedProfitFormatted} ${coin}`}
                 valueClassName={isWin ? "text-green-500" : "text-red-500"}
               />
@@ -142,6 +172,7 @@ export function TradeInfoDialog() {
 
               <InfoRow
                 label={t("total_funding")}
+                tooltipInfo={t("total_funding_info")}
                 value={`${totalFundingFormatted} ${coin}`}
                 valueClassName={getResultClass(totalFunding)}
               />
@@ -153,7 +184,8 @@ export function TradeInfoDialog() {
               />
 
               <InfoRow
-                label={t("aprox_entry_amount")}
+                label={t("approx_entry_amount")}
+                tooltipInfo={t("approx_entry_amount_info")}
                 value={`≈ ${aproxEntryAmountWithLeverage} ${coin}`}
               />
 
@@ -176,6 +208,10 @@ export function TradeInfoDialog() {
                 label={`${t("close_position_amount")} (${leverage}x)`}
                 value={`≈ ${closePositionAmountFormatted} ${formattedSymbol}`}
               />
+
+              <p className="text-sm text-gray-400 mt-4">
+                * {t("not_exact_values_description")}
+              </p>
             </div>
           </div>
           {/* <DialogFooter>
