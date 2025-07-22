b918e86e5aba386e93d32d2700a0bfc16303c2ee
Ruben <rubdeivis@gmail.com>
2025-07-22 09:21:18 -0400

    refactor: change profit factor tooltips

--- src/features/dashboard/components/ProfitFactor.tsx
+++ src/features/dashboard/components/ProfitFactor.tsx
@@ -1,8 +1,9 @@
 import { formatDecimal } from "@/utils/number-utils";
 import { useTranslations } from "next-intl";
-import { Cell, Pie, PieChart, ResponsiveContainer } from "recharts";
+import { Cell, Pie, PieChart, ResponsiveContainer, Tooltip } from "recharts";
 import { TradeStatisticsResult } from "@/features/trades/interfaces/trades-interfaces";
 import { StatisticCard } from "./StatisticCard";
+import { useUserConfigStore } from "@/store/user-config-store";
 
 interface ProfitFactorProps {
   profitFactor: TradeStatisticsResult["profitFactor"];
@@ -10,6 +11,7 @@ interface ProfitFactorProps {
 
 export function ProfitFactor({ profitFactor }: ProfitFactorProps) {
   const t = useTranslations("dashboard.statistics");
+  const { coin } = useUserConfigStore();
 
   return (
     <StatisticCard
@@ -21,30 +23,36 @@ export function ProfitFactor({ profitFactor }: ProfitFactorProps) {
         </p>
       }
       rightContent={
-        <div className="min-w-[170px]">
+        <div className="col-span-1">
           <ResponsiveContainer
+            className="-mt-6"
             width="100%"
-            height={160}
-            className="scale-35 -mt-13"
+            height="100%"
+            minHeight={100}
           >
             <PieChart>
               <Pie
                 data={[
                   {
-                    name: "wins",
+                    name: t("win"),
                     value: profitFactor?.sumWin,
                   },
                   {
-                    name: "losses",
+                    name: t("lost"),
                     value: profitFactor?.sumLoss,
                   },
                 ]}
-                innerRadius={60}
-                outerRadius={80}
+                innerRadius="60%"
+                outerRadius="80%"
                 dataKey="value"
               >
                 <Cell strokeWidth={0} fill={"var(--color-green-500)"} />
                 <Cell strokeWidth={0} fill={"var(--color-red-500)"} />
+                <Tooltip
+                  formatter={(value) =>
+                    `${formatDecimal(Number(value) || 0)} ${coin}`
+                  }
+                />
               </Pie>
             </PieChart>
           </ResponsiveContainer>
