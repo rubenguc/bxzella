ac81900315d60a0724d09117227f8ed07c598b5a
Ruben <rubdeivis@gmail.com>
2025-11-27 10:01:06 -0400

    refactor: remove outline selection and showing values in profits by day

--- src/features/dashboard/components/day-profits-chart.tsx
+++ src/features/dashboard/components/day-profits-chart.tsx
@@ -5,6 +5,7 @@ import {
   Bar,
   BarChart,
   Cell,
+  LabelList,
   ResponsiveContainer,
   Tooltip,
   XAxis,
@@ -38,10 +39,6 @@ export function DayProfitsChart({
   const { coin, dayProfitsChartMode, setDayProfitsChartMode } =
     useUserConfigStore();
 
-  if (!data || data.length === 0) {
-    return <div>{t("noData")}</div>;
-  }
-
   const gradientOffset = () => {
     const dataMax = Math.max(...data.map((i) => i.profit));
     const dataMin = Math.min(...data.map((i) => i.profit));
@@ -67,6 +64,28 @@ export function DayProfitsChart({
     data,
   };
 
+  const CustomLabel = (props: any) => {
+    const { x, y, value } = props;
+    const numericValue = Number(value);
+    const formattedValue = formatDecimal(numericValue, 2);
+    const textColor = getBarColor(numericValue);
+
+    const dy = numericValue >= 0 ? -10 : 10;
+
+    return (
+      <text
+        x={x}
+        y={y}
+        dy={dy}
+        textAnchor="middle"
+        fill={textColor}
+        fontSize={10}
+      >
+        {formattedValue}
+      </text>
+    );
+  };
+
   const commonAxisProps = (
     <>
       <XAxis
@@ -76,7 +95,7 @@ export function DayProfitsChart({
         tick={{ textAnchor: "middle" }}
         tickMargin={10}
       />
-      <YAxis width="auto" />
+      <YAxis width="auto" domain={["auto", (dataMax) => dataMax * 1.15]} />
       <Tooltip
         wrapperClassName="bg-card! text-white!"
         formatter={(profit, index) => [
@@ -112,53 +131,68 @@ export function DayProfitsChart({
           </Select>
         </CardAction>
       </CardHeader>
-      <CardContent className="h-[420px]">
-        <ResponsiveContainer width="100%" height="100%">
-          {dayProfitsChartMode === "area" ? (
-            <AreaChart {...commonChartProps}>
-              {commonAxisProps}
-              <defs>
-                <linearGradient id="splitColor" x1="0" y1="0" x2="0" y2="1">
-                  <stop
-                    offset="0"
-                    stopColor="var(--color-green-500)"
-                    stopOpacity={1}
-                  />
-                  <stop
-                    offset={off}
-                    stopColor="var(--color-green-500)"
-                    stopOpacity={0.1}
-                  />
-                  <stop
-                    offset={off}
-                    stopColor="var(--color-red-500)"
-                    stopOpacity={0.1}
-                  />
-                  <stop
-                    offset="1"
-                    stopColor="var(--color-red-500)"
-                    stopOpacity={1}
-                  />
-                </linearGradient>
-              </defs>
-              <Area
-                type="monotone"
-                dataKey="profit"
-                stroke="#fff"
-                fill="url(#splitColor)"
-              />
-            </AreaChart>
-          ) : (
-            <BarChart {...commonChartProps}>
-              {commonAxisProps}
-              <Bar dataKey="profit">
-                {data.map(({ profit }, index) => (
-                  <Cell key={index.toString()} fill={getBarColor(profit)} />
-                ))}
-              </Bar>
-            </BarChart>
-          )}
-        </ResponsiveContainer>
+      <CardContent className="h-[420px] outline-0!">
+        {data.length === 0 ? (
+          <div>
+            <span>{t("no_data_to_show")}</span>
+          </div>
+        ) : (
+          <ResponsiveContainer>
+            {dayProfitsChartMode === "area" ? (
+              <AreaChart {...commonChartProps} className="ring-0">
+                {commonAxisProps}
+                <defs>
+                  <linearGradient id="splitColor" x1="0" y1="0" x2="0" y2="1">
+                    <stop
+                      offset="0"
+                      stopColor="var(--color-green-500)"
+                      stopOpacity={1}
+                    />
+                    <stop
+                      offset={off}
+                      stopColor="var(--color-green-500)"
+                      stopOpacity={0.1}
+                    />
+                    <stop
+                      offset={off}
+                      stopColor="var(--color-red-500)"
+                      stopOpacity={0.1}
+                    />
+                    <stop
+                      offset="1"
+                      stopColor="var(--color-red-500)"
+                      stopOpacity={1}
+                    />
+                  </linearGradient>
+                </defs>
+                <Area
+                  type="monotone"
+                  dataKey="profit"
+                  stroke="#fff"
+                  fill="url(#splitColor)"
+                >
+                  <LabelList content={CustomLabel} />
+                </Area>
+              </AreaChart>
+            ) : (
+              <BarChart {...commonChartProps}>
+                {commonAxisProps}
+                <Bar
+                  dataKey="profit"
+                  label={{
+                    position: "top",
+                    fontSize: 10,
+                    formatter: (value) => formatDecimal(Number(value), 2),
+                  }}
+                >
+                  {data.map(({ profit }, index) => (
+                    <Cell key={index.toString()} fill={getBarColor(profit)} />
+                  ))}
+                </Bar>
+              </BarChart>
+            )}
+          </ResponsiveContainer>
+        )}
       </CardContent>
     </Card>
   );
